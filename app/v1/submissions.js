dfd9c2e6aa1926f63baf71ab9c8c01fcab7dfbea
André König <andre.koenig@posteo.de>
2015-04-06 11:05:47 +0200

    typo.

--- app/v1/submissions.js
+++ app/v1/submissions.js
@@ -85,7 +85,7 @@ SubmissionsResource.prototype.insert = function insert (req, res) {
 
     // TODO: Refactor use JSON schema
     if (!submission.authorName ||
-        !submission.authorInfo ||
+        //!submission.authorInfo ||
         !submission.organizationName ||
         !submission.organizationInfo ||
         !submission.cahootsSource ||
